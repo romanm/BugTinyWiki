cb1a2dcb26d1e0784672d13c703947763f446204
roman.mishchenko@gmail.com <roman.mishchenko@gmail.com>
2015-09-05 22:40:32 +0300

    Right page number addition.

--- src/main/java/hello/AppConfig.java
+++ src/main/java/hello/AppConfig.java
@@ -2,9 +2,9 @@ package hello;
 
 public class AppConfig {
 	//development
-	final static String applicationFolderPfad	= "/home/roman/algoritmed.com/bug-cms/BugTinyWiki/";
-	final static String applicationDbFolderPfad	= "/home/roman/algoritmed.com/bug-cms/db/";
+//	final static String applicationFolderPfad	= "/home/roman/algoritmed.com/bug-cms/BugTinyWiki/";
+//	final static String applicationDbFolderPfad	= "/home/roman/algoritmed.com/bug-cms/db/";
 	//prodaction
-//	final static String applicationFolderPfad	= "/home/holweb/server4/BugTinyWiki/";
-//	final static String applicationDbFolderPfad	= "/home/holweb/server4/db/";
+	final static String applicationFolderPfad	= "/home/holweb/server4/BugTinyWiki/";
+	final static String applicationDbFolderPfad	= "/home/holweb/server4/db/";
 }
