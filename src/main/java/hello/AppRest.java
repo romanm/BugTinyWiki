85595b5cd2d98d5f255fa664ecef0ab884b86ab1
roman.mishchenko@gmail.com <roman.mishchenko@gmail.com>
2015-09-06 11:14:12 +0300

    add 3 html as samples

--- src/main/java/hello/AppRest.java
+++ src/main/java/hello/AppRest.java
@@ -1,5 +1,10 @@
 package hello;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,4 +32,16 @@ public class AppRest {
 		return appService.readJsonFromFile(bugTinyWikiJsonFileName);
 	}
 	//------------BugTinyWiki------------------------------------END
+
+	@RequestMapping(value = "/page1/readJsonFromRam", method = RequestMethod.GET)
+	public  @ResponseBody Map<String, Object> readJsonFromRam() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("text", "Hello World");
+		map.put("int", 123);
+		map.put("date", new Date());
+		List<Integer> intList = new ArrayList<Integer>(Arrays.asList(new Integer[]{1,2,3}));
+		map.put("intList", intList);
+		return map;
+	}
+
 }
